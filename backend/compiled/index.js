ca0d3cca82a2b7e59830d1bf0fb66bbe3415c9a1
Vince-maple-byte <73848683+Vince-maple-byte@users.noreply.github.com>
2024-03-29 01:42:15 -0400

    Figured out on how to create a http response based on whether someone submitted a post request

--- backend/compiled/index.js
+++ backend/compiled/index.js
@@ -3,6 +3,23 @@ const http = require('http');
 const port = 3000;
 const host = "localhost";
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    //console.log(req.url + ": "+ req.method);
+    //Example of sending a http response from a post request
+    let json = {
+        data: 'Hellow'
+    };
+    if (req.method == 'POST') {
+        console.log('Post request');
+        //Makes the header based on whatever Content-Type that we want, but
+        //Since we are sending a json data we use application/json
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        //res.end just specifies what we are sending to the body
+        res.end(JSON.stringify({
+            data: json.data,
+        }));
+    }
+    else {
+        res.end('Hello world');
+    }
 });
 server.listen(port, () => console.log(`Server listening on port ${port}`));
--- backend/index.ts
+++ backend/index.ts
@@ -2,8 +2,32 @@ const http = require('http');
 const port: number = 3000;
 const host: string = "localhost";
 
+//Learned how we can create objects in typescript by specifying the object data types
+type responseData = {
+    data: string;
+}
+
 const server = http.createServer((req: any, res: any) => {
-    console.log(req.url, req.method);
+    //console.log(req.url + ": "+ req.method);
+    //Example of sending a http response from a post request
+    let json : responseData= {
+        data: 'Hellow'
+    };
+
+    if(req.method == 'POST'){
+        console.log('Post request');
+        //Makes the header based on whatever Content-Type that we want, but
+        //Since we are sending a json data we use application/json
+        res.writeHead(200, {'Content-Type':'application/json'});
+        //res.end just specifies what we are sending to the body
+        res.end(JSON.stringify({
+            data: json.data,
+        }));
+    }
+    else{
+        res.end('Hello world');
+    }
+    
 });
 
 server.listen(port, () => console.log(`Server listening on port ${port}`));
